90d9a13800406225b74bb0822cdc1ca8376d26d6
Joppy Furr <joppyfurr@gmail.com>
2020-01-19 21:31:40 +1300

    Assembler: More instructions :D

--- Assembler/main.c
+++ Assembler/main.c
@@ -345,6 +345,11 @@ do \
 #define PARSE_REG(reg, ptr) \
 do \
 { \
+    if (ptr[0] != 'r') \
+    { \
+        fprintf (stderr, "Error: Invalid register \"%s\".\n", ptr); \
+        return -1; \
+    } \
     ptr [2] = '\0'; \
     reg = strtoul (& ptr [1], NULL, 16) - 1; \
     if (reg >= 4) \
@@ -364,6 +369,13 @@ do \
     } \
 } while (0)
 
+#define UNEXPECTED(input) \
+do \
+{ \
+    fprintf (stderr, "Error: Unexpected input \"%s\".\n", input); \
+    return -1; \
+} while (0)
+
 
 
 int parse_asm (FILE *source)
@@ -374,6 +386,7 @@ int parse_asm (FILE *source)
 
     uint8_t dst;
     uint8_t src;
+    uint16_t value;
 
     while (fscanf (source, "%s", buffer) != EOF)
     {
@@ -384,20 +397,18 @@ int parse_asm (FILE *source)
             {
                 SCAN_NEXT_TOKEN ();
             } while (strcmp ("*/", buffer) != 0);
-
-            continue;
         }
 
         /* Offset */
-        if (strncmp (".", buffer, 1) == 0)
+        else if (strncmp (".", buffer, 1) == 0)
         {
             PARSE_HEX_INT (address, & buffer [1]);
-
-            continue;
         }
 
+        /* TODO: Label */
+
         /* mov */
-        if (strcmp ("mov", buffer) == 0)
+        else if (strcmp ("mov", buffer) == 0)
         {
             SCAN_NEXT_TOKEN ();
 
@@ -409,7 +420,6 @@ int parse_asm (FILE *source)
                 PARSE_REG (src, buffer);
                 rom [address++] = MOV_R1_R1 + (dst << 2) + (src << 0);
             }
-
             /* mov dh, rX */
             else if (strncmp ("dh", buffer, 2) == 0)
             {
@@ -417,7 +427,6 @@ int parse_asm (FILE *source)
                 PARSE_REG (src, buffer);
                 rom [address++] = MOV_DH_R1 + (src << 0);
             }
-
             /* mov dl, rX */
             else if (strncmp ("dl", buffer, 2) == 0)
             {
@@ -425,7 +434,6 @@ int parse_asm (FILE *source)
                 PARSE_REG (src, buffer);
                 rom [address++] = MOV_DL_R1 + (src << 0);
             }
-
             /* mov dc, hl */
             else if (strncmp ("dc", buffer, 2) == 0)
             {
@@ -433,7 +441,6 @@ int parse_asm (FILE *source)
                 CONFIRM_MATCH ("hl", buffer);
                 rom [address++] = MOV_DC_HL;
             }
-
             /* mov sp, hl */
             else if (strncmp ("sp", buffer, 2) == 0)
             {
@@ -441,20 +448,341 @@ int parse_asm (FILE *source)
                 CONFIRM_MATCH ("hl", buffer);
                 rom [address++] = MOV_SP_HL;
             }
-
-            continue;
+            else
+            {
+                UNEXPECTED (buffer);
+            }
         }
 
         /* ldi */
-        /* ld */
-        /* st */
-        /* pop */
-        /* push */
-        /* jmp */
-        /* call */
-        /* ret */
+        else if (strcmp ("ldi", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
 
-        fprintf (stderr, "Warning: Unexpected input \"%s\".\n", buffer);
+            /* ldi rX, 0xXX */
+            if (strncmp ("r", buffer, 1) == 0)
+            {
+                PARSE_REG (dst, buffer);
+
+                SCAN_NEXT_TOKEN ();
+                PARSE_INT (value, buffer);
+                rom [address++] = LDI_R1_XX + (dst << 0);
+                rom [address++] = (uint8_t) value;
+            }
+            /* ldi hl, 0xXXXX */
+            else if (strncmp ("hl", buffer, 2) == 0)
+            {
+                uint16_t value = 0;
+
+                SCAN_NEXT_TOKEN ();
+                PARSE_INT (value, buffer);
+                rom [address++] = LDI_HL_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+            else
+            {
+                UNEXPECTED (buffer);
+            }
+        }
+
+        /* ld */
+        else if (strcmp ("ld", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            if (strncmp ("r", buffer, 1) == 0)
+            {
+                PARSE_REG (dst, buffer);
+
+                SCAN_NEXT_TOKEN ();
+
+                /* ld rX, dc */
+                if (strcmp ("dc", buffer) == 0)
+                {
+                    rom [address++] = LD_R1_DC + (dst << 0);
+                }
+                /* ld rX, [dh, rX] */
+                else if (strncmp ("[dh", buffer, 3) == 0)
+                {
+                    SCAN_NEXT_TOKEN ();
+                    PARSE_REG (src, buffer);
+                    rom [address++] = LD_R1_DH_R1 + (dst << 2) + (src << 0);
+                }
+                /* ld rX, [0xXXXX] */
+                else if (strncmp ("[", buffer, 1) == 0)
+                {
+                    SCAN_NEXT_TOKEN ();
+                    PARSE_INT (value, buffer);
+                    rom [address++] = LD_R1_XXXX + (dst << 0);
+                    rom [address++] = (uint8_t) (value >> 8);
+                    rom [address++] = (uint8_t) value;
+                }
+                else
+                {
+                    UNEXPECTED (buffer);
+                }
+            }
+            else
+            {
+                UNEXPECTED (buffer);
+            }
+        }
+
+        /* st */
+        else if (strcmp ("st", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* st dc, rX */
+            if (strcmp ("dc", buffer) == 0)
+            {
+                SCAN_NEXT_TOKEN ();
+                PARSE_REG (src, buffer);
+                rom [address++] = ST_DC_R1 + (src << 0);
+            }
+            /* st [dh, rX], rx */
+            else if (strncmp ("[dh", buffer, 3) == 0)
+            {
+                SCAN_NEXT_TOKEN ();
+                PARSE_REG (dst, buffer);
+                SCAN_NEXT_TOKEN ();
+                PARSE_REG (src, buffer);
+                rom [address++] = ST_DH_R1_R1 + (dst << 2) + (src << 0);
+            }
+            /* st [0xXXXX], rX */
+            else if (strncmp ("[", buffer, 1) == 0)
+            {
+                PARSE_INT (value, & buffer [1]);
+                SCAN_NEXT_TOKEN ();
+                PARSE_REG (src, buffer);
+                rom [address++] = ST_XXXX_R1+ (src << 0);
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+            else
+            {
+                UNEXPECTED (buffer);
+            }
+        }
+
+        /* pop rX */
+        else if (strcmp ("pop", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            rom [address++] = POP_R1 + (dst << 0);
+        }
+
+        /* push rX */
+        else if (strcmp ("push", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = PUSH_R1 + (src << 0);
+        }
+
+        /* jmp */
+        else if (strcmp ("jmp", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* jmp hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = JMP_HL;
+            }
+            /* jmp 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = JMP_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+        else if (strcmp ("jmp-z", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* jmp-z hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = JMP_Z_HL;
+            }
+            /* jmp-z 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = JMP_Z_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+        else if (strcmp ("jmp-nz", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* jmp-nz hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = JMP_NZ_HL;
+            }
+            /* jmp-nz 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = JMP_NZ_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+        else if (strcmp ("jmp-neg", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* jmp-neg hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = JMP_NEG_HL;
+            }
+            /* jmp-neg 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = JMP_NEG_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+        else if (strcmp ("jmp-pos", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* jmp-pos hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = JMP_POS_HL;
+            }
+            /* jmp-pos 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = JMP_POS_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+
+        /* call */
+        else if (strcmp ("call", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+
+            /* call hl */
+            if (strcmp ("hl", buffer) == 0)
+            {
+                rom [address++] = CALL_HL;
+            }
+            /* call 0xXXXX */
+            else
+            {
+                PARSE_INT (value, buffer);
+                rom [address++] = CALL_XXXX;
+                rom [address++] = (uint8_t) (value >> 8);
+                rom [address++] = (uint8_t) value;
+            }
+        }
+
+        /* ret */
+        else if (strcmp ("ret", buffer) == 0)
+        {
+            rom [address++] = RET;
+        }
+
+        /* TODO: I/O */
+        /* TODO cfg-set / cfg-clr */
+
+        /* add */
+        else if (strcmp ("add", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = ADD_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* addc */
+        else if (strcmp ("addc", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = ADDC_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* sub */
+        else if (strcmp ("sub", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = SUB_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* subc */
+        else if (strcmp ("subc", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = SUBC_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* and */
+        else if (strcmp ("and", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = AND_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* or */
+        else if (strcmp ("or", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = OR_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* xor */
+        else if (strcmp ("xor", buffer) == 0)
+        {
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (dst, buffer);
+            SCAN_NEXT_TOKEN ();
+            PARSE_REG (src, buffer);
+            rom [address++] = XOR_R1_R1 + (dst << 2) + (src << 0);
+        }
+
+        /* TODO: lshift */
+        /* TODO: rshift */
+
+        else
+        {
+#if 0
+            UNEXPECTED (buffer);
+#endif
+            fprintf (stderr, "Warning: Unexpected input \"%s\".\n", buffer);
+        }
     }
 }
 
